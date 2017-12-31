62ac2e143c478079426f23714bb99d367dece8e1
ClarenceNeo <niuzhiyuan94@gmail.com>
2017-12-31 14:29:58 +0800

    change login

--- public/js/public/login.js
+++ public/js/public/login.js
@@ -8,12 +8,33 @@
   function init() {
     el_form.addEventListener('submit', function (e) {
       e.preventDefault();
-      $.post('/api/user/login', el_form.get_data())
-        .then(function () {
-          location.href = '/';
-        }, function () {
-          alert('用户名或密码有误');
+
+      $.ajax({
+        url: '/api/user/login',
+        method: 'post',
+        data: el_form.get_data(),
+        cache: false,
+        contentType: false,
+        processData: false
+      })
+        .then(function (r) {
+          if (r.success) {
+            // me.read();
+            location.href = '/';
+          }
+          // me.list = r.data;
+          // if (me.after_add)
+          //   me.after_add();
+
+          // return r;
         })
+
+      // $.post('/api/user/login', el_form.get_data())
+      //   .then(function () {
+      //     
+      //   }, function () {
+      //     alert('用户名或密码有误');
+      //   })
     })
   }
 
